e205915505c38bc1da3bf83c667e471c33104fe6
dacarlson6 <dacarlson6@wisc.edu>
2024-07-24 06:48:53 -0500

    add data and circles for data

--- js/main.js
+++ js/main.js
@@ -26,4 +26,47 @@ window.onload = function(){
         .style("fill", "#FFFFFF"); //fill color
 
     console.log(innerRect);
+
+    //bubble chart data
+    var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    //create circles for each city
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //feed in an array
+        .enter() //one of the great mysteries of the universe :)
+        .append("circle") //add a circle for each datum
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
+        })
+        .attr("r", function(d){
+            //calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i){
+            //use the index to place each circle horizontally
+            return 90 + (i * 180);
+        })
+        .attr("cy", function(d){
+            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+            return 450 - (d.population * 0.0005);
+        });
 };
