662d110e8078ae241e5c6f1801d3c79ef70f1332
dacarlson6 <dacarlson6@wisc.edu>
2024-08-04 10:17:44 -0500

    debug color

--- js/main.js
+++ js/main.js
@@ -63,92 +63,94 @@
             //join csv data to GeoJSON enumeration units
             franceRegions = joinData(franceRegions, csvData);
 
+            //create a color scale
+            var colorScale = makeColorScale(csvData);
+
             //add Europe countries to map
             var countries = map.append("path")
                 .datum(europeCountries)
                 .attr("class", "countries")
                 .attr("d", path);           
 
-            //add enumeration units to the map
-            setEnumerationUnits(franceRegions, map, path);
+            //add enumeration units to the map with color scale
+            setEnumerationUnits(franceRegions, map, path, colorScale);
         };
 
     }; //end of setMap()
 
-        function setGraticule(map, path){
-            //...GRATICULE BLOCKS FROM CHAPTER 8
-        };        
+    function setGraticule(map, path){
+        //...GRATICULE BLOCKS FROM CHAPTER 8
+    };        
 
-        //function to join csv data to GeoJSON
-        function joinData(franceRegions, csvData) {
-            //variables for data join
-            var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+    //function to join csv data to GeoJSON
+    function joinData(franceRegions, csvData) {
+        //variables for data join
+        var attrArray = ["varA", "varB", "varC", "varD", "varE"];
 
-            //loop through csv to assign each set of csv attribute values to geojson region
-            for (var i = 0; i < csvData.length; i++) {
-                var csvRegion = csvData[i]; //the current region
-                var csvKey = csvRegion.adm1_code; //the CSV primary key
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i = 0; i < csvData.length; i++) {
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.adm1_code; //the CSV primary key
 
-                //loop through geojson regions to find correct region
-                for (var a = 0; a < franceRegions.length; a++) {
-                    var geojsonProps = franceRegions[a].properties; //the current region geojson properties
-                    var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+            //loop through geojson regions to find correct region
+            for (var a = 0; a < franceRegions.length; a++) {
+                var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
 
-                    //where primary keys match, transfer csv data to geojson properties object
-                    if (geojsonKey === csvKey) {
-                        //assign all attributes and values
-                        attrArray.forEach(function(attr) {
-                            var val = parseFloat(csvRegion[attr]); //get csv attribute value
-                            geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                        });
-                    }
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey === csvKey) {
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr) {
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
                 }
             }
-            return franceRegions;
         }
+        return franceRegions;
+    }
 
-        //Example 1.3 line 38
-        function setEnumerationUnits(franceRegions, map, path, colorScale){
+    function setEnumerationUnits(franceRegions, map, path, colorScale) {
+        //add France regions to map
+        var regions = map.selectAll(".regions")
+            .data(franceRegions)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path)
+            .style("fill", function(d){
+                var value = d.properties[expressed];
+                return value ? colorScale(value) : "#ccc"; //assign neutral color if no value
+            });
+    }
 
-            //add France regions to map
-            var regions = map.selectAll(".regions")
-                .data(franceRegions)
-                .enter()
-                .append("path")
-                .attr("class", function(d){
-                    return "regions " + d.properties.adm1_code;
-                })
-                .attr("d", path)
-                .style("fill", function(d){
-                    return colorScale(d.properties[expressed]);
-                });
-        }
+    //function to create color scale generator
+    function makeColorScale(data){
+        var colorClasses = [
+            "#D4B9DA",
+            "#C994C7",
+            "#DF65B0",
+            "#DD1C77",
+            "#980043"
+        ];
 
-        //function to create color scale generator
-        function makeColorScale(data){
-            var colorClasses = [
-                "#D4B9DA",
-                "#C994C7",
-                "#DF65B0",
-                "#DD1C77",
-                "#980043"
-            ];
+        //create color scale generator
+        var colorScale = d3.scaleQuantile()
+            .range(colorClasses);
 
-            //create color scale generator
-            var colorScale = d3.scaleQuantile()
-                .range(colorClasses);
-
-            //build array of all values of the expressed attribute
-            var domainArray = [];
-            for (var i=0; i<data.length; i++){
-                var val = parseFloat(data[i][expressed]);
-                domainArray.push(val);
-            };
-
-            //assign array of expressed values as scale domain
-            colorScale.domain(domainArray);
-
-            return colorScale;
+        //build array of all values of the expressed attribute
+        var domainArray = [];
+        for (var i=0; i<data.length; i++){
+            var val = parseFloat(data[i][expressed]);
+            domainArray.push(val);
         };
+
+        //assign array of expressed values as scale domain
+        colorScale.domain(domainArray);
+
+        return colorScale;
+    };
 
 })(); //last line of main.js
