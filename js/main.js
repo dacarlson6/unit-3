c8124a4780c9097fc8db0a44b135f1702aefb335
dacarlson6 <dacarlson6@wisc.edu>
2024-08-04 09:00:28 -0500

    join csv and your geojson features

--- js/main.js
+++ js/main.js
@@ -44,9 +44,12 @@ function setMap() {
         console.log(europe);
         console.log(france);
 
-        //translate europe TopoJSON
+         //translate europe and France TopoJSONs
         var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
-            franceRegions = topojson.feature(france, france.objects.FranceRegions);
+            franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
+
+        //join csv data to GeoJSON enumeration units
+        franceRegions = joinData(franceRegions, csvData);
 
         //add Europe countries to map
         var countries = map.append("path")
@@ -63,5 +66,33 @@ function setMap() {
                 return "regions " + d.properties.adm1_code;
             })
             .attr("d", path);
+    }
+
+    //function to join csv data to GeoJSON
+    function joinData(franceRegions, csvData) {
+        //variables for data join
+        var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i = 0; i < csvData.length; i++) {
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.adm1_code; //the CSV primary key
+
+            //loop through geojson regions to find correct region
+            for (var a = 0; a < franceRegions.length; a++) {
+                var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey === csvKey) {
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr) {
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                }
+            }
+        }
+        return franceRegions;
     }
 };
